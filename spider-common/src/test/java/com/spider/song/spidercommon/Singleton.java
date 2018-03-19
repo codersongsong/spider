85d26383a91bf1da67db1e0d028b0aa6f243de1c
songzhengjie <songzhengjie@outlook.com>
2018-03-19 11:30:41 +0800

    branch0302:单链表合并

--- spider-common/src/test/java/com/spider/song/spidercommon/Singleton.java
+++ spider-common/src/test/java/com/spider/song/spidercommon/Singleton.java
@@ -13,8 +13,48 @@ public class Singleton {
     private static class SingletonHolder {
         private static final Singleton INSTANCE = new Singleton();
     }
-    private Singleton (){}
+
+    private Singleton() {
+    }
+
     public static final Singleton getInstance() {
         return SingletonHolder.INSTANCE;
     }
+
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public Node mergeNode(Node head1, Node head2) {
+
+        Node node1 = head1;
+        Node node2 = head2;
+        Node node3 = null;
+        while (node1 != null && node2 != null) {
+
+            node1 = node1.next;
+
+
+            while (node2.next.data <= node1.data) {
+                node3.next = node2.next;
+                node3 = node3.next;
+                node2 = node2.next;
+            }
+            node3.next = node1;
+            node3 = node3.next;
+        }
+
+        return node3;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
